c4c3967db8e30b6ce22f73251476c75729589961
danielson317 <daniel@quickemr.com>
2018-12-19 12:36:02 -0700

    Sanitation fix

--- log/log.pg.php
+++ log/log.pg.php
@@ -49,7 +49,7 @@ function logView()
         $type,
         $timestamp,
         htmlSolo('input', array('type' => 'button', 'value' => 'expand', 'class' => array('expand'))),
-        '<pre>' . $entry . '</pre>',
+        '<pre>' . sanitize($entry) . '</pre>',
       );
 
       // Limit results to the last 100 entries.
