617948983124520436e4ac3284193f16b419a8ef
alee4 <austin.ellis.lee@gmail.com>
2023-02-08 16:39:41 -0500

    added form sections, date, datetime, timezone added analog clock functionality fixed bug with repeating times

--- time/time.js
+++ time/time.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
     start();
     setInterval(start, 1000);
 
+    startClockHands()
+    setInterval(startClockHands, 1000)
+
 });
 
 function start() {
@@ -55,4 +58,22 @@ function start() {
     $('.alaska').html(alaska);
     $('.hawaii').html(hawaii);
 
+}
+function startClockHands(){
+    const now = new Date();
+    const secondHand = document.querySelector('.second-hand');
+    const minsHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
--- time/time.pg.php
+++ time/time.pg.php
@@ -4,10 +4,9 @@ function timezonePage()
     //setup file, use template and add file paths
     $template = new HTMLTemplate();
     $template->setTitle('Epoch Converter');
-//  $template->addCssFilePath('/time/time.css');
+    $template->addCssFilePath('/time/time.css');
     $template->addJsFilePath('/time/time.js');
     $template->setMenu(menu());
-
 
 //  $code_snippet = htmlWrap('script', $js);
     $now = new DateTime();
@@ -20,19 +19,78 @@ function timezonePage()
     $myEpochTime = htmlWrap("div", "", array('class' => array('unix-time')));
 
     $timeZones = "";
-    $timeZones .= htmlWrap("ul", "", array('class' => array('eastern')));
-    $timeZones .= htmlWrap("ul", "", array('class' => array('central')));
-    $timeZones .= htmlWrap("ul", "", array('class' => array('mountain')));
-    $timeZones .= htmlWrap("ul", "", array('class' => array('pacific')));
-    $timeZones .= htmlWrap("ul", "", array('class' => array('alaska')));
-    $timeZones .= htmlWrap("ul", "", array('class' => array('hawaii')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('eastern')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('central')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('mountain')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('pacific')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('alaska')));
+    $timeZones .= htmlWrap("li", "", array('class' => array('hawaii')));
+    $timeZones .= htmlWrap("ul", "", array('class' => array('timeZoneList')));
+
+    //create new form
+    $form = new Form('json_formatter');
+
+    $group = 'top_group';
+    $form->addGroup($group);
+
+    //date select field
+    $field = new FieldDate('date', 'Date');
+//  $field->setValue('01/08/2023');
+    $form->addField($field);
+
+    //time select dropdown field
+    $field = new FieldTime('time', 'Datetime');
+//  $field->setValue('01/08/2023');
+    $form->addField($field);
+
+    $list = array(
+        'America/New_York' => 'Eastern',
+        'America/Chicago' => 'Central',
+        'America/Denver' => 'Mountain',
+        'America/Phoenix' => 'Mountain (Arizona)',
+        'America/Los_Angeles' => 'Pacific',
+        'America/Anchorage' => 'Alaska',
+        'America/Adak' => 'Hawaii',
+        'Pacific/Honolulu' => 'Hawaii (No Daylight Savings)',
+    );
+
+    $field = new FieldSelect('timezone', 'Timezone', $list);
+    $field->setValue('America/Chicago');
+    $form->addField($field);
+//    $formSection = htmlWrap('div', $form, array('class' => array('formSection')));
+
+    $fullClockHTMLWrap = createClock('eastern_standard', 'EST') . createClock('central', 'cst');
 
 
-
+    $clockSection = htmlWrap('ul', $fullClockHTMLWrap, array('class' => array('clockSection')));
 
     //$body = htmlWrap('p','test .php file html wrap ' . time() . ' ' . $pretty);
-    $header = "Epoch Converter";
-    $template->setBody(htmlWrap('h1', $header) . $body . $myEpochTime . $timeZones);
-
+    $header = 'Epoch Converter';
+    $template->setBody(htmlWrap('h1', $header) . $body . $myEpochTime . $timeZones . $form . $clockSection);
     return $template;
+}
+
+function createClock($timezone, $timezoneLabel)
+{
+    //clock face
+    $labelWrap = htmlWrap('h2', $timezoneLabel, array('class' => array('timezone-header')));
+
+    $face = htmlWrap('div',NULL, array('class' => array('marking marking-one')));
+    $face .= htmlWrap('div',NULL, array('class' => array('marking marking-two')));
+    $face .= htmlWrap('div',NULL, array('class' => array('marking marking-three')));
+    $face .= htmlWrap('div',NULL, array('class' => array('marking marking-four')));
+
+    $face = htmlWrap('div', $face, array('class' => array('outer-clock-face')));
+
+    //clock hands
+    $hands = htmlWrap('div', NULL, array('class' => array('hand hour-hand')));
+    $hands .= htmlWrap('div', NULL, array('class' => array('hand min-hand')));
+    $hands .= htmlWrap('div', NULL, array('class' => array('hand second-hand')));
+    $innerFace = htmlWrap('div', $hands, array('class' => array('inner-clock-face')));
+
+    $fullClockHTMLWrap = $labelWrap . htmlWrap('div',  $face . $innerFace, array('id' => array('timezone_' . $timezone), 'class' => array('clock')));
+
+    //$fullClockHTMLWrap .= htmlWrap("li",$fullClockHTMLWrap, Null);
+
+    return $fullClockHTMLWrap;
 }
